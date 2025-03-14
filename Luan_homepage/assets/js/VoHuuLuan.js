ae82ddc311bdb4c838a5e438e84ef20f9197ad2d
Vo Huu Luan <119402962+Soar06@users.noreply.github.com>
2025-03-14 14:36:11 +0700

    Thêm tất cả code mới cho nhánh luan_overview

---
+++ Luan_homepage/assets/js/VoHuuLuan.js
@@ -0,0 +1,12 @@
+// Theme Toggle
+document.querySelector('.theme-toggle').addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme');
+    const icon = document.querySelector('.theme-toggle i');
+    if (document.body.classList.contains('dark-theme')) {
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+    } else {
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
+    }
+});
